1c72eda0f5796240e326639512215c82dbf7d566
ezgaro <ezgaro2005@gmail.com>
2024-07-09 10:45:35 +0300

    partials and layout functionality

--- NodeJs/ExpressJS and Templating/hbs-demo/index.js
+++ NodeJs/ExpressJS and Templating/hbs-demo/index.js
@@ -1,9 +1,35 @@
-const app = require('express')();
+const app = require("express")();
+const handlebars = require("express-handlebars");
 
-
-
-app.get('/', (req, res) => {
-  res.send("It works");
+//making the extension to be .hbs not .handlebars
+const hbs = handlebars.create({
+  extname: ".hbs",
 });
+
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+
+let visitors = 0;
+
+let products = [
+  { name: "Widget", price: 49 },
+  { name: "Gadjet", price: 19 },
+  { name: "Fluxor", price: 13 },
+];
+
+app.get("/", (req, res) => {
+  res.locals = {
+    count: visitors++,
+  };
+  res.render("home");
+});
+
+app.get('/catalog', (req, res) => {
+  res.locals = {
+    products
+  }
+  res.render('catalog');
+})
+
 
 app.listen(3000);
