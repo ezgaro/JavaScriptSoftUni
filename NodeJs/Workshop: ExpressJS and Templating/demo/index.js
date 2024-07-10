7a3fdf5ddb986b7df1d63721083e6709c0636fe6
ezgaro <ezgaro2005@gmail.com>
2024-07-10 11:54:51 +0300

    Adding our controllers and our first middleware

--- NodeJs/Workshop: ExpressJS and Templating/demo/index.js
+++ NodeJs/Workshop: ExpressJS and Templating/demo/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const { home } = require('./controllers/home');
+const { about } = require('./controllers/about');
+const { create } = require('./controllers/create');
+const { details } = require('./controllers/details');
+const { notFound } = require('./controllers/notFound');
+
+const carsService = require('./services/cars');
 
 const app = express();
 
@@ -13,8 +19,14 @@ app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true}));
 app.use('/static', express.static('static'));
 
+app.use(carsService());
 
 app.get('/', home);
+app.get('/about', about);
+app.get('/create', create);
+app.get('/details/:id', details);
+
+app.all('*', notFound);
 
 
 app.listen(3000 , () => console.log('Server start'));
