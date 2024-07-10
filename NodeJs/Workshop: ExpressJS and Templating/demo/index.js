7559187a7a568f8349ff252176bc8042f4d6aeb3
ezgaro <ezgaro2005@gmail.com>
2024-07-10 11:20:14 +0300

    Adding all the views that we will use

---
+++ NodeJs/Workshop: ExpressJS and Templating/demo/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const { home } = require('./controllers/home');
+
+const app = express();
+
+app.engine('hbs', hbs.create({
+  extname:'.hbs'
+}).engine);
+
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended: true}));
+app.use('/static', express.static('static'));
+
+
+app.get('/', home);
+
+
+app.listen(3000 , () => console.log('Server start'));
